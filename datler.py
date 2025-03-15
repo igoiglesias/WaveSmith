2ce756504e45cd6dec48fe046e21731f9c5da2df
Igor Iglesias <contato@igoriglesias.com>
2025-03-15 13:49:50 -0300

    Melhorias de performance no cuttler e datler

--- datler.py
+++ datler.py
@@ -6,12 +6,19 @@ import os
 import torch
 import whisper
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device: {device}")
+MODEL = None
 
+
+def load_model(model_size: str) -> None:
+    global MODEL
+    if not MODEL:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Using device: {device}")
+        MODEL = whisper.load_model(model_size, device=device)
+    return MODEL
 
 def transcribe_audio(input_path: str, model_size: str, language: str) -> str:
-    model = whisper.load_model(model_size, device=device)    
+    model = load_model(model_size)  
     audio = whisper.load_audio(input_path)
     audio = whisper.pad_or_trim(audio)
     result = model.transcribe(audio, language=language)
@@ -23,7 +30,8 @@ def process_directory(input_dir: str, output_dir: str, language: str, model_size
     os.makedirs(wavs_dir, exist_ok=True)
 
     metadata_file = os.path.join(output_dir, "metadata.csv")
-    for file in Path(input_dir).glob("*.wav"):
+    files = Path(input_dir).glob("*.wav")
+    for file in sorted(files):
         print(f"Processing file: {file.name}")
         
         transcription = transcribe_audio(file, model_size, language)
@@ -33,17 +41,17 @@ def process_directory(input_dir: str, output_dir: str, language: str, model_size
 
         with open(metadata_file, mode='a', newline='', encoding='utf-8') as f:
             writer = csv.writer(f, delimiter='|')
-            writer.writerow([file_name, transcription, transcription])
+            writer.writerow([file_name.strip(), transcription.strip(), transcription.strip()])
 
     print(f"Dataset saved at: {output_dir}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Transcrição de áudio para dataset no padrão LJSpeech")
-    parser.add_argument("input", help="Caminho do diretório com arquivos .wav de entrada")
-    parser.add_argument("--output", default="dataset", help="Caminho do diretório de saída para o dataset")
-    parser.add_argument("--language", default="pt", help="Idioma para transcrição")
+    parser = argparse.ArgumentParser(description="Audio transcription for a dataset in LJSpeech format")
+    parser.add_argument("input", help="Path to the directory containing input .wav files")
+    parser.add_argument("--output", default="dataset", help="Path to the output directory for the dataset")
+    parser.add_argument("--language", default="pt", help="Language for transcription")
     parser.add_argument("--model_size", choices=["tiny", "base", "small", "medium", "large"], default="medium", 
-                        help="Escolha o tamanho do modelo Whisper (tiny, base, small, medium, large). Padrão: base")
+                        help="Choose the Whisper model size (tiny, base, small, medium, large). Default: base")
 
     args = parser.parse_args()
     process_directory(
